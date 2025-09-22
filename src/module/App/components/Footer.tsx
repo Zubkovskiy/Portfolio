65e98e11ffabce560fb3519b3122e9b5248c3f47
Zubkovskiy Bogdan <zubkovvsbogdan@gmail.com>
2025-09-22 15:03:11 +0300

    add RotatingText

--- src/module/App/components/Footer.tsx
+++ src/module/App/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="absolute bottom-0 left-0 right-0 bg-[var(--color-background)] py-4 sm:py-8">
+    <footer className="absolute bottom-0 left-0 right-0 mb-4 bg-[var(--color-background)] py-2">
       <div className="container mx-auto text-center">
         <p className="text-sm text-gray-500">
           &copy; 2025 My Portfolio. All rights reserved.
--- src/module/App/components/Header.tsx
+++ src/module/App/components/Header.tsx
@@ -2,6 +2,7 @@ import classNames from "classnames";
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router";
 import { RunningLine } from "../../shared/components/RunningLine";
+import RotatingText from "../../shared/components/RotatingText";
 
 export const Header = () => {
   const [scrolled, setScrolled] = useState(false);
@@ -43,9 +44,22 @@ export const Header = () => {
       >
         <NavLink
           to="/"
-          className="text-3xl font-bold text-primary leading-[0] cursor-pointer"
+          className="group flex items-center gap-4 text-3xl font-bold text-primary leading-none cursor-pointer"
         >
-          ZBS
+          <p className="group-hover:text-[var(--color-text)] duration-300">ZBS</p>
+
+          <RotatingText
+            texts={["Zubkovskyi", "Bohdan", "Serhiyovych"]}
+            mainClassName="opacity-0 max-w-0 group-hover:opacity-100 group-hover:max-w-xs transition-all duration-500 overflow-hidden justify-center rounded-lg"
+            staggerFrom="last"
+            initial={{ y: "100%" }}
+            animate={{ y: 0 }}
+            exit={{ y: "-120%" }}
+            staggerDuration={0.025}
+            splitLevelClassName="overflow-hidden"
+            transition={{ type: "spring", damping: 30, stiffness: 400 }}
+            rotationInterval={2000}
+          />
         </NavLink>
 
         <ul className="space-x-6 hidden md:flex">
@@ -143,117 +157,4 @@ export const Header = () => {
       </aside>
     </>
   );
-  // return (
-  //   <>
-  //     <div
-  //       className={classNames(
-  //         "sticky flex justify-between items-center w-full h-15 py-4 px-8 rounded-2xl z-[100] transition-all duration-600",
-  //         scrolled
-  //           ? "top-4 backdrop-blur-md bg-[var(--color-primary)]/4 shadow-md"
-  //           : "top-3 bg-transparent"
-  //       )}
-  //       id="header"
-  //     >
-  //       <NavLink
-  //         to="/"
-  //         className="text-3xl font-bold text-primary leading-[0] cursor-pointer"
-  //       >
-  //         ZBS
-  //       </NavLink>
-
-  //       <ul className="space-x-6 hidden md:flex">
-  //         <li>
-  //           <NavLink className={getNavLinkClass} to="/">
-  //             About
-  //           </NavLink>
-  //         </li>
-  //         <li>
-  //           <NavLink className={getNavLinkClass} to="/projects">
-  //             Projects
-  //           </NavLink>
-  //         </li>
-  //         <li>
-  //           <NavLink className={getNavLinkClass} to="/contact">
-  //             Contact
-  //           </NavLink>
-  //         </li>
-  //       </ul>
-
-  //       <button
-  //         onClick={() => setMenuOpen((prev) => !prev)}
-  //         className="md:hidden flex flex-col gap-2 w-8 ml-auto cursor-pointer z-[110]"
-  //       >
-  //         <div
-  //           className={classNames(
-  //             "rounded-2xl h-[3px] w-1/2 bg-[var(--color-primary)] duration-500",
-  //             menuOpen &&
-  //               "rotate-[225deg] origin-right -translate-x-[12px] -translate-y-[1px]"
-  //           )}
-  //         ></div>
-  //         <div
-  //           className={classNames(
-  //             "rounded-2xl h-[3px] w-full bg-[var(--color-primary)] duration-500",
-  //             menuOpen && "-rotate-45"
-  //           )}
-  //         ></div>
-  //         <div
-  //           className={classNames(
-  //             "rounded-2xl h-[3px] w-1/2 bg-[var(--color-primary)] duration-500 place-self-end",
-  //             menuOpen &&
-  //               "rotate-[225deg] origin-left translate-x-[12px] translate-y-[1px]"
-  //           )}
-  //         ></div>
-  //       </button>
-  //     </div>
-
-  //     <div className="fixed inset-0 z-[90] pointer-events-none">
-  //       <div
-  //         className={classNames(
-  //           "absolute inset-0 bg-[var(--color-primary)] transform transition-transform duration-500",
-  //           menuOpen ? "translate-x-0 delay-0" : "translate-x-full delay-200"
-  //         )}
-  //       ></div>
-
-  //       <div
-  //         className={classNames(
-  //           "absolute inset-0 bg-[var(--color-background)] transform transition-transform duration-500",
-  //           menuOpen ? "translate-x-0 delay-200" : "translate-x-full delay-0"
-  //         )}
-  //       ></div>
-  //     </div>
-
-  //     <aside
-  //       className={classNames(
-  //         "fixed top-0 right-0 w-full h-full z-[95] flex justify-center items-center transform transition-transform duration-500",
-  //         menuOpen ? "translate-x-0 delay-260" : "translate-x-full delay-0"
-  //       )}
-  //     >
-  //       <nav className="flex flex-col justify-center items-center gap-6 text-3xl">
-  //         <NavLink
-  //           className={getNavLinkClass}
-  //           to="/"
-  //           onClick={() => setMenuOpen(false)}
-  //         >
-  //           About
-  //         </NavLink>
-  //         <NavLink
-  //           className={getNavLinkClass}
-  //           to="/projects"
-  //           onClick={() => setMenuOpen(false)}
-  //         >
-  //           Projects
-  //         </NavLink>
-  //         <NavLink
-  //           className={getNavLinkClass}
-  //           to="/contact"
-  //           onClick={() => setMenuOpen(false)}
-  //         >
-  //           Contact
-  //         </NavLink>
-  //       </nav>
-
-  //       <RunningLine main />
-  //     </aside>
-  //   </>
-  // );
 };
