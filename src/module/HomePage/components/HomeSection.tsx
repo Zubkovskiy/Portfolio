79368b547ea1de9c9bced6fdd54e98f41c779e63
Zubkovskiy Bogdan <zubkovvsbogdan@gmail.com>
2025-12-05 15:37:24 +0200

    Add Download cv in HomeSection

--- src/module/HomePage/components/HomeSection.tsx
+++ src/module/HomePage/components/HomeSection.tsx
@@ -14,22 +14,35 @@ export const HomeSection = () => {
           Hello There!
         </span>
       </div>
+
       <h1 className="text-center mt-2 sm:mt-6 text-3xl sm:text-4xl font-bold">
         I'm Zubkovskiy Bohdan,
       </h1>
+
       <h2 className="mt-1 sm:mt-2.5 text-center text-2xl sm:text-4xl text-primary font-bold">
         Front-end Developer
       </h2>
+
       <p className="mt-2 sm:mt-8 mx-auto text-center text-md sm:text-2xl max-w-200 opacity-70">
         I am a ambitious Frontend developer with a great desire to create
         creative web applications and interactive user interfaces.
       </p>
-      <Link
-        to="contact"
-        className="flex items-center justify-center mt-4 sm:mt-8 border-2 w-30 sm:w-40 h-10 sm:h-12 border-[var(--color-primary)] rounded-lg text-xl font-semibold hover:bg-[var(--color-primary)] hover:text-[var(--color-background)] transition-colors duration-300 cursor-pointer"
-      >
-        Let's Talk
-      </Link>
+
+      <div className="flex gap-8">
+        <Link
+          to="contact"
+          className="flex items-center justify-center mt-4 sm:mt-8 border-2 w-30 sm:w-40 h-10 sm:h-12 border-[var(--color-primary)] rounded-lg text-xl font-semibold hover:bg-[var(--color-primary)] hover:text-[var(--color-background)] transition-colors duration-300 cursor-pointer"
+        >
+          Let's Talk
+        </Link>
+        <a
+          href="/assets/Bohdan_Zubkovskiy.pdf"
+          download="Bohdan_Zubkovskiy.pdf"
+          className="flex items-center justify-center mt-4 sm:mt-8 border-2 w-30 sm:w-40 h-10 sm:h-12 border-[var(--color-primary)] rounded-lg text-xl font-semibold hover:bg-[var(--color-primary)] hover:text-[var(--color-background)] transition-colors duration-300 cursor-pointer"
+        >
+          Download CV
+        </a>
+      </div>
       <RunningLine main />
     </section>
   );
